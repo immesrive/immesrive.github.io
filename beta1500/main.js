b2adeb0bb6bb4b4e56a3e66511a2d427b7e63788
dungnguyen234 <dung.ntk234@gmail.com>
2022-11-22 01:12:58 +0700

    change model demo

--- beta1500/main.js
+++ beta1500/main.js
@@ -15,9 +15,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-    const raccoon = await loadGLTF('./assets/models/musicband-raccoon/scene.gltf');
-    raccoon.scene.scale.set(0.1, 0.1, 0.1);
-    raccoon.scene.position.set(0, -0.4, 0);
+    const raccoon = await loadGLTF('./assets/models/Booth.gltf');
+    raccoon.scene.scale.set(0.2, 0.2, 0.2);
+    raccoon.scene.position.set(0.6,0, 0);
 
     const anchor = mindarThree.addAnchor(0);
     anchor.group.add(raccoon.scene);
